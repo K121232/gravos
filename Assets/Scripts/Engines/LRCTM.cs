b99e7dca03e4f2ca778cb0e479858f0d14ccd719
K121232 <104253515+K121232@users.noreply.github.com>
2023-03-09 00:06:13 -0800

    Working LRCTM
    Although it needs a lot of tuning for each type of engine, it is working in theory at least

--- Assets/Scripts/Engines/LRCTM.cs
+++ Assets/Scripts/Engines/LRCTM.cs
@@ -9,40 +9,54 @@ public class LRCTM : TM {
     public  int             major = 0;
     public  float           minor = 0;
 
+    public  float           STRV;
+
     public  Vector2 Gitmas ( int a2, float a1 ) {
         return points [ a2 ] * ( 1 - a1 ) * ( 1 - a1 ) * ( 1 - a1 ) + 3 * ( ( 1 - a1 ) * ( 1 - a1 ) * a1 * ( points [ a2 ] + points [ a2 + 1 ] * ( a2 % 4 == 0 ? 1 : -1 ) ) + ( 1 - a1 ) * a1 * a1 * ( points [ a2 + 2 ] + points [ a2 + 3 ] ) ) + a1 * a1 * a1 * points [ a2 + 2 ];
     }
 
-    public override void Update () {
-        float minA = 180;
-        float delta3 = 0;
-
-        float   deltaMIN = minor;
-        int     deltaMAJ = major;
-
+    private void DrawCourse () {
         Vector2 delta = points [ 0 ], delta2;
         for ( int i = 0; i < points.Length - 3; i += 2 ) {
             if ( resolution <= 0 ) return;
             for ( float j = 0; j <= 1; j += resolution ) {
                 delta2 = Gitmas ( i, j );
-
                 Debug.DrawLine ( delta, delta2 );
-
-                if ( Mathf.Abs ( ( major - i + points.Length - 4 ) % ( points.Length - 4 ) + minor - j ) < scanDistance ) {
-                    delta3 = Vector2.Angle ( delta - delta2, (Vector2) transform.position - delta2 );
-                    if ( j != 0 && delta3 < minA ) {
-                        minA = delta3;
-                        deltaMAJ = i;
-                        deltaMIN = j;
-                    }
-                }
-                major = deltaMAJ;
-                minor = deltaMIN;
                 delta = delta2;
             }
         }
-        Debug.DrawLine ( transform.position, Gitmas ( major, minor ), Color.red );
+    }
+
+    public override void Update () {
+        DrawCourse ();
+
+
+        float   bestScore = Mathf.Infinity;
+        Vector2 deltaA, deltaB = new Vector2 ( major, minor );
+        Vector2 target = transform.position + (Vector3)rgb.velocity * STRV;
+
+        Vector2 DEBUGP = Vector2.zero;
+
+        for ( float i = 0; i < scanDistance; i += resolution ) {
+            int md = ( major + Mathf.FloorToInt ( i + minor ) * 2 ) % ( points.Length - 2 );
+            deltaA = Gitmas ( md, minor + i - Mathf.Floor ( minor + i ) );
+            if ( ( target - deltaA ).sqrMagnitude < bestScore ) {
+                bestScore = ( target - deltaA ).sqrMagnitude;
+                deltaB = new Vector2 ( md, minor + i - Mathf.Floor ( minor + i ) );
+            }
+            if ( i != 0 ) {
+                Debug.DrawLine ( DEBUGP, deltaA, Color.magenta );
+            }
+            DEBUGP = deltaA;
+        }
+
+        major = Mathf.RoundToInt ( deltaB.x );
+        minor = deltaB.y;
+
         targetLink = Gitmas ( major, minor );
+
+        Debug.DrawLine ( target, targetLink, Color.red );
+
         base.Update ();
     }
 }
