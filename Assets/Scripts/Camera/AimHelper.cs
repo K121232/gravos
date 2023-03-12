fdb1e1effc5c81beb42850b392706254b7913d0d
K121232 <104253515+K121232@users.noreply.github.com>
2023-03-12 14:26:09 -0700

    Aim helper
    Exactly what's on the box

---
+++ Assets/Scripts/Camera/AimHelper.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class AimHelper : MonoBehaviour {
+    private CameraTether    tether;
+    private Camera          cam;
+    
+    public  Rigidbody2D     bodyA;
+    public  Rigidbody2D     bodyB;
+
+    public  float           STRPD;      // Position delta
+    public  float           STRORS;     // Ortographic size
+
+    private Vector3         originalOffset;
+    private float           originalSize;
+
+    public  float           maxOrtoSize;
+
+    private void Start () {
+        cam = GetComponent<Camera> ();
+        tether = GetComponent<CameraTether> ();
+        originalOffset = tether.offset;
+        originalSize = cam.orthographicSize;
+    }
+
+    void LateUpdate () {
+        tether.offset = originalOffset + ( bodyB.transform.position - bodyA.transform.position ) * STRPD;
+        cam.orthographicSize = Mathf.Min ( originalSize + ( bodyB.transform.position - bodyA.transform.position ).magnitude * STRORS, maxOrtoSize );
+    }
+}
