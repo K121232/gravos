43f2f590a85491f553b896505f3e5ce29771772b
K121232 <104253515+K121232@users.noreply.github.com>
2023-03-13 12:26:18 -0700

    Briefing Menu

---
+++ Assets/Scripts/GUI/Menus/BriefingMenu.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BriefingMenu : MenuCore {
+    public  GameObject[]    pages;
+    private int             iter;
+
+    public  Button          buttonP;
+    public  Button          buttonN;
+
+    public override void Incoming ( bool a ) {
+        iter = 0;
+        FlipPage ( 0 );
+        base.Incoming ( a ); 
+    }
+
+    public  void    FlipPage ( int alpha ) {
+        pages [ iter ].SetActive ( false );
+        iter += alpha;
+        if ( iter >= pages.Length ) {
+            iter = pages.Length - 1;
+        }
+        if ( iter < 0 ) {
+            iter = 0;
+        }
+        buttonP.interactable = iter != 0;
+        buttonN.interactable = iter != pages.Length - 1;
+
+        pages [ iter ].SetActive ( true );
+    }
+
+    public void Update () {
+        if ( Input.GetKeyDown ( KeyCode.B ) ) {
+            Backflow ( !status );
+        }
+    }
+}
