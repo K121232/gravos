ecbc2e58bdea513d1378005fa1311d0c4cb6df2f
K121232 <104253515+K121232@users.noreply.github.com>
2023-03-12 17:18:46 -0700

    Ship hud acceleration movement

--- Assets/Scripts/GUI/ShipHUD.cs
+++ Assets/Scripts/GUI/ShipHUD.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ShipHUD : MonoBehaviour {
+    public  RectTransform       tRoot;
     public  ProtoPlayerBridge   bridge;
     public  Slider              hpSlider;
 
@@ -11,6 +12,10 @@ public class ShipHUD : MonoBehaviour {
     public  PowerCell           corvoCell;
     public  Slider              timeSlider;
 
+    public  float               STRV;
+    public  float               STRV2;
+    private Vector2             pastV;
+    private Rigidbody2D         rgb;
 
     private void SetScale ( Transform sliderRoot, float aScale ) {
         sliderRoot.GetChild ( 1 ).GetChild ( 0 ).GetComponent<Image> ().pixelsPerUnitMultiplier = aScale * 0.44f;
@@ -23,11 +28,15 @@ public class ShipHUD : MonoBehaviour {
         SetScale ( hpSlider.transform, bridge.baseHealth );
         SetScale ( shieldSlider.transform, shieldCell.resourceMax / bridge.STRShield );
         SetScale ( timeSlider.transform, corvoCell.resourceMax );
+
+        rgb = bridge.movement.GetComponent<Rigidbody2D> ();
     }
 
     void LateUpdate () {
         hpSlider.value      = bridge.GetProcentHP () * hpSlider.maxValue;
         shieldSlider.value  = shieldCell.GetAvailableLoad () * shieldSlider.maxValue;
         timeSlider.value    = corvoCell.GetAvailableLoad () * timeSlider.maxValue;
+        tRoot.rotation = Quaternion.FromToRotation ( (Vector3) rgb.velocity - (Vector3) pastV + new Vector3 ( 0, 0, STRV ), Vector3.forward );
+        pastV = Vector2.Lerp ( pastV, rgb.velocity, STRV2 * Time.deltaTime );
     }
 }
