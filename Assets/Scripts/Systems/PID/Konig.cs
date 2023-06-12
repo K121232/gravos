38ee40346feebe9bc21a8466e91f38d98960b757
K121232 <104253515+K121232@users.noreply.github.com>
2023-06-12 11:43:45 +0300

    Konig filter removed the persistent error problem

--- Assets/Scripts/Systems/PID/Konig.cs
+++ Assets/Scripts/Systems/PID/Konig.cs
@@ -5,15 +5,26 @@ public class Konig : MonoBehaviour {
     public float Ki;
     public float Kd;
 
-    private float integral;
-    private float previousError;
+    private float   integral;
+    
+    private int     filterSize = 10, filterIndex = 0;
+    private float[] integralFilter;
+
+    private float   previousError;
 
     // MAJOR WARNING : MUST BE UPDATED IN LOCKSTEP WITH THE TARGET, OR IT LOSES SYNC
+
+    private void Start () {
+        integralFilter = new float [ filterSize ];
+        Reset ();
+    }
 
     public float NextFrame ( float target, float current, float deltaTime ) {
         if ( deltaTime == 0 ) { return 0; }
         float error = target - current;
-        integral += error * deltaTime;
+        
+        UpdateIntegral ( error * deltaTime );
+
         float derivative = (error - previousError) / deltaTime;
 
         float control = (Kp * error) + (Ki * integral) + (Kd * derivative);
@@ -25,5 +36,18 @@ public class Konig : MonoBehaviour {
     public void Reset () {
         integral = 0f;
         previousError = 0f;
+        integralFilter = new float [ filterSize ];
+        for ( int i = 0; i < filterSize; i++ ) {
+            integralFilter[ i ] = 0;
+        }
+        filterIndex = 0;
+    }
+
+    public void UpdateIntegral ( float _alpha ) {
+        Debug.Log ( filterIndex );
+        integral -= integralFilter [ filterIndex ];
+        integralFilter [ filterIndex ] = _alpha / filterSize;
+        integral += integralFilter [ filterIndex ];
+        filterIndex = ( filterIndex + 1 ) % filterSize;
     }
 }
