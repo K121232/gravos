4789c65e1212cd77cdb39bb560071a049f964938
K121232 <104253515+K121232@users.noreply.github.com>
2023-07-08 17:26:15 +0300

    Fixed the persistent trail renderer

--- Assets/Scripts/Systems/Payloads/PayloadTrailDeleter.cs
+++ Assets/Scripts/Systems/Payloads/PayloadTrailDeleter.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
 
 public class PayloadTrailDeleter : PayloadCore {
+    public TrailRenderer    trailRenderer;
+
     public override void Deploy ( PayloadObject _instructions = null ) {
         base.Deploy ( _instructions );
         PassOn ();
     }
 
     public override void Store () {
-        if ( instructions != null ) {
-            TrailRenderer trr =  instructions.controllerRoot.GetChild( 0 ).GetComponentInChildren<TrailRenderer>();
-            if ( trr != null ) {
-                trr.Clear ();
+        if ( trailRenderer != null ) {
+            if ( trailRenderer != null ) {
+                trailRenderer.Clear ();
             }
         }
         base.Store ();
